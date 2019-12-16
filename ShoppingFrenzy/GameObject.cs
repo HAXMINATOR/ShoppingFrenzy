9a57ddacf09cd0c4aeac6279d4af509e84a77c64
HAXMINATOR <thor_e_jensen@hotmail.com>
2019-12-16 12:27:56 +0100

    added gameobject

---
+++ ShoppingFrenzy/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingFrenzy
+{
+    class GameObject
+    {
+        public Texture2D sprite;
+        public Vector2 position;
+        public Color color = Color.White;
+
+        public void Update()
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, color);
+        }
+    }
+}
--- ShoppingFrenzy/Shopper.cs
+++ ShoppingFrenzy/Shopper.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace ShoppingFrenzy
 {
-    class Shopper
+    class Shopper : GameObject
     {
     }
 }
